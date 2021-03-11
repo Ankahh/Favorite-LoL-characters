207778e7cb3aca7e1e5277f2feeb0667ebf09189
Ankahh <61977498+Ankahh@users.noreply.github.com>
2021-03-11 01:05:46 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,46 @@
-function ahri() {
-    var valasz = confirm("anyád picsája");
-    if (valasz = true) {
-        
+var valasz;
+
+function ahri() 
+{
+    valasz = prompt("Hány éves vagy?");
+    if (valasz >= 18) 
+    {
+        alert('Rendben, tovább mehetsz.');
     }
 
-    else {
-        
+    else 
+    {
+        alert('Sajnálom, de még nem múltál el 18.')
+
     }
 }
 
-function akali() {
+function akali() 
+{
+    valasz = prompt("Hány éves vagy?");
+    if (valasz >= 18) 
+    {
+        alert('Rendben, tovább mehetsz.');
+    }
 
+    else 
+    {
+        alert('Sajnálom, de még nem múltál el 18.')
+
+    }
 }
 
-function seraphine() {
+function seraphine() 
+{   
+    valasz = prompt("Hány éves vagy?");
+    if (valasz >= 18) 
+    {
+        alert('Rendben, tovább mehetsz.');
+    }
 
+    else 
+    {
+        alert('Sajnálom, de még nem múltál el 18.')
+
+    }
 }
